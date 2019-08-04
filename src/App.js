fe2be945d4ff573978de3971ba52c255d0269e11
Daniel Carlos <danielboll.academico@gmail.com>
2019-08-04 05:02:24 -0300

    Added theme configuartion

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import './css/App.css';
 import Routes from './components/routes/Routes';
 import NavBar from './components/widgets/NavBar';
+import {MuiThemeProvider, createMuiTheme} from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#4caf50',
+      main: '#357a38',
+      dark: '#6fbf73',
+      shade: 500
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <MuiThemeProvider theme={theme}>
+        <React.Fragment>
         <NavBar />
         <Routes />
       </React.Fragment >
+      </MuiThemeProvider>
     );
   }
 }
